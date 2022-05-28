a96f69b333cecae1680a6b742de3f2a8052b9f6a
Admin 水野 <mizno911.zeta@gmail.com>
2022-05-28 11:20:19 +0900

    jquery後読みコードの追加

---
+++ js/jquery_read.js
@@ -0,0 +1,7 @@
+if (!window.jQuery) {
+  document.body.appendChild((function(){
+    var s = document.createElement("script");
+    s.src = '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js';
+    return s;
+  })());
+}
